ffea0c2fc2910f0164f8be99058c7ce19b9e76d7
dhairya <dhairyagoel777@gmail.com>
2026-04-23 11:50:32 +0530

    updates

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://ai-fds-mse2.onrender.com/api' });
 
 // Attach token to every request if present
 API.interceptors.request.use((config) => {
